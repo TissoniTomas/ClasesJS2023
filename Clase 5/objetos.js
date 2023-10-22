3056256582337ef11420b5236ac0b7c338e7b7ed
TissoniTomas <tissonitomas5@gmail.com>
2023-10-22 17:00:42 -0300

    Trabajamos clase 5

--- Clase 5/objetos.js
+++ Clase 5/objetos.js
@@ -4,9 +4,9 @@
 
 // Objetos
 
-let nombre ="Homero";
-let edad = "39";
-let calle = "Av Siempre Viva"
+// let nombre ="Homero";
+// let edad = "39";
+// let calle = "Av Siempre Viva"
 
 const persona1 = { nombre: "Homero", edad: "39", calle: "Av Siempre Viva",
 
@@ -19,6 +19,11 @@ console.log(persona1.nombre);
 console.log(persona1.calle);
 console.log(persona1.edad);
 
+// let pedirDato = prompt("Ingrese la propiedad a solicitar");
+// console.log(pedirDato);
+// console.log(persona1.pedirDato);
+// // En caso de que la persona solicite la propiedad del objeto correctamente, este se mostrara.
+
 // Asignar valores
 
 console.log(persona1["nombre"] = "Marge");
@@ -29,27 +34,85 @@ console.log(persona1.edad = 38);
 console.log(persona1.comidaFav = "Donnuts");
 console.log(persona1);
 
-// Constructores
+// Constructores (Empiezan con mayuscula por ser constructoras)
 
-function Persona(nombre,edad,calle){
-  this.nombre = nombre;
-  this.edad = edad;
-  this.calle = calle;
+// function Persona(nombre,edad,calle){
+//   this.nombre = nombre;
+//   this.edad = edad;
+//   this.calle = calle;
 
-  // Metodo
-  this.hablar = function(){
-    console.log("Hola soy " + this.nombre);
+//   // Metodo
+//   this.hablar = function(){
+//     console.log("Hola soy " + this.nombre);
+//   }
+//   this.nombreYEdad  = function(separador){
+//     console.log(
+//       this.nombre + separador + this.edad);
+//     }
+
+// }
+
+// const persona2 = new Persona("Lisa" , 8 , "Av SiempreViva");
+// console.log(persona2);
+
+// let nombrePersona3 = prompt("Ingrese su nombre");
+// let callePersona3 = prompt("Ingrese su calle");
+// let edadPersona3 = prompt("Ingrese su edad");
+
+
+// const persona3 = new Persona( nombrePersona3, callePersona3, edadPersona3);
+
+// console.log(persona3);
+
+// let auto = "Ford"
+
+// console.log(auto.length);
+// console.log(auto.toLocaleUpperCase());
+// console.log(auto.toLocaleLowerCase());
+  
+// persona2.hablar() // Llamo al metodo del objeto constructor
+
+// persona2.nombreYEdad(" ")
+
+// // IN FOR IN
+
+// console.log("nombre" in persona2);
+// // console.log(("dni" in persona2));
+// if(!("dni" in persona2)){
+//   persona2.dni = "54.252.333"
+// }
+
+// for (const propiedad in persona2){
+//   console.log(persona2[propiedad])
+//     if(propiedad ==="calle")
+//     break;
+//   ;
+// }
+
+// CLASES CONSTRUCTORAS
+
+class Auto{
+  constructor(marca,modelo,anio,precio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio;
+    this.precio = precio
   }
-  this.nombreYEdad  = function(separador){
-    this.nombre + separador + this.edad
-  }
 
+    // Metodos en class
+    sumarIva(){
+      return this.precio = this.precio * 1.21
+    }
 }
 
-const persona2 = new Persona("Lisa" , 8 , "Av SiempreViva");
-console.log(persona2);
+const auto1 = new Auto();
 
-let auto = "ford"
+auto1.marca = prompt("Ingrese la marca de su auto");
+auto1.modelo = prompt("Ingrese el modelo de su auto");
+auto1.anio = prompt("Ingrese el año de su auto");
+auto1.precio = prompt("Ingrese el precio de su auto")
 
-console.log(auto.length);
-console.log(auto.toLocaleUpperCase());
+console.log("A continuacion vera los datos de su auto");
+
+console.log(auto1);
+console.log("El precio de su auto con el IVA es de " + auto1.sumarIva());
